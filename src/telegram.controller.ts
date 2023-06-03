06140744285c80429895382fc8ac3df2ce393c8f
Vadim Kushir <vadim.kushnir@logity.tech>
2023-06-04 00:56:19 +0600

    fix: removed fallback get endpoint

--- src/telegram.controller.ts
+++ src/telegram.controller.ts
@@ -5,13 +5,8 @@ import { TelegramService } from './telegram.service';
 export class TelegramController {
   constructor(private readonly telegramService: TelegramService) {}
 
-  // @Post()
-  // async handleUpdate(@Body() update: any): Promise<any> {
-  //   await this.telegramService.handleUpdate(update);
-  // }
-
-  @Get()
-  handleUpdate(): String {
-    return 'xui vo rtu'
+  @Post()
+  async handleUpdate(@Body() update: any): Promise<any> {
+    await this.telegramService.handleUpdate(update);
   }
 }
