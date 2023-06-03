cf3f5f47995588d69ac1124246c72cc2144af2c9
Vadim Kushir <vadim.kushnir@logity.tech>
2023-06-04 00:33:54 +0600

    fix: temp add get req

--- src/telegram.controller.ts
+++ src/telegram.controller.ts
@@ -1,12 +1,17 @@
-import { Controller, Post, Body } from '@nestjs/common';
+import { Controller, Post, Body, Get } from '@nestjs/common';
 import { TelegramService } from './telegram.service';
 
 @Controller('telegram')
 export class TelegramController {
   constructor(private readonly telegramService: TelegramService) {}
 
-  @Post()
-  async handleUpdate(@Body() update: any): Promise<any> {
-    await this.telegramService.handleUpdate(update);
+  // @Post()
+  // async handleUpdate(@Body() update: any): Promise<any> {
+  //   await this.telegramService.handleUpdate(update);
+  // }
+
+  @Get()
+  handleUpdate(): String {
+    return 'xui vo rtu'
   }
 }
